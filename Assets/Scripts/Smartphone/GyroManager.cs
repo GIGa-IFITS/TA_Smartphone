8c2e4982f5b2d4269409bb4d479d510aa14289db
anggunw <56246449+anggunw@users.noreply.github.com>
2022-05-12 10:48:42 +0700

    update gyro correction

--- Assets/Scripts/Smartphone/GyroManager.cs
+++ Assets/Scripts/Smartphone/GyroManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GyroManager : MonoBehaviour
 {
     private Gyroscope gyro;
-    private Quaternion correctionQuaternion = Quaternion.Euler(90, 90, 0);
+    public Quaternion correctionQuaternion = Quaternion.Euler(90, 88, 0);
     private Quaternion phoneRotation;
 
     void Start()
@@ -17,7 +17,9 @@ public class GyroManager : MonoBehaviour
     void Update()
     {
         Quaternion gyroQuaternion = GyroToUnity(Input.gyro.attitude);
+        Debug.Log(gyroQuaternion);
         phoneRotation = correctionQuaternion * gyroQuaternion;
+        //phoneRotation = gyroQuaternion;
         ClientSend.SendRotation(phoneRotation.x, phoneRotation.y, phoneRotation.z, phoneRotation.w);
     }
 
